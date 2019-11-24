8e7f631a11c4f204b7b498797981ad946676e17e
Albert Sunyer <albert@nye.cat>
2019-11-24 01:41:24 +0100

    Electron settings

--- src/electron.js
+++ src/electron.js
@@ -1,39 +1,102 @@
 const { app, BrowserWindow, Menu } = require('electron');
-const path = require('path')
+const path = require('path');
+const settings = require('electron-settings');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let windowState = {};
 
 // Create simple menu for easy devtools access, and for demo
-const menuTemplateDev = [
+const applicationMenu = [
 	{
-	  label: 'Options',
+	  label: app.name,
 	  submenu: [
 		{
-		  label: 'Open Dev Tools',
-		  click() {
-			mainWindow.openDevTools();
-		  },
+			label: 'About ' + app.name,
+			role: 'about'
 		},
+		/*{
+			type: 'separator'
+		},
+		{
+			label: 'Open Dev Tools',
+			click() {
+				mainWindow.openDevTools();
+			},
+		},*/
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Quit',
+			accelerator: 'CmdOrCtrl+Q',
+			click: () => { app.quit(); }
+		}
 	  ],
 	},
+	{
+		label: 'Edit',
+		submenu: [{
+		  label: 'Undo',
+		  accelerator: 'CmdOrCtrl+Z',
+		  selector: 'undo:'
+		}, {
+		  label: 'Redo',
+		  accelerator: 'Shift+CmdOrCtrl+Z',
+		  selector: 'redo:'
+		}, {
+		  type: 'separator'
+		}, {
+		  label: 'Cut',
+		  accelerator: 'CmdOrCtrl+X',
+		  selector: 'cut:'
+		}, {
+		  label: 'Copy',
+		  accelerator: 'CmdOrCtrl+C',
+		  selector: 'copy:'
+		}, {
+		  label: 'Paste',
+		  accelerator: 'CmdOrCtrl+V',
+		  selector: 'paste:'
+		}, {
+		  label: 'Select All',
+		  accelerator: 'CmdOrCtrl+A',
+		  selector: 'selectAll:'
+		}]
+	  }
   ];
 
 function createWindow() {
 	const mode = process.env.NODE_ENV;
+
+	windowState = settings.get('windowState', {});
 
     mainWindow = new BrowserWindow({
 		webPreferences: {
 			nodeIntegration: false,
 			preload: __dirname + '/preload.js'
 		},
+		title: app.name,
+  		icon: __dirname + '/img/icon.png',
 		titleBarStyle: 'hidden',
-        height: 920,
-    	width: 1600,
+        width: windowState.bounds && windowState.bounds.width || 1600,
+		height: windowState.bounds && windowState.bounds.height || 920,
+		x: windowState.bounds && windowState.bounds.x || undefined,
+		y: windowState.bounds && windowState.bounds.y || undefined,
+		backgroundColor: '#FFFFFF',
+		show: false
 	});
 
-	if (process.env.NODE_ENV === 'development') {
+	if(settings.get('isMaximized')){
+		mainWindow.maximize();
+	}
+
+	if (process.platform === 'darwin') {
+		Menu.setApplicationMenu(Menu.buildFromTemplate(applicationMenu));
+	}
+
+	if (mode === 'development') {
 		// Set our above template to the Menu Object if we are in development mode, dont want users having the devtools.
 		//Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplateDev));
 		// If we are developers we might as well open the devtools by default.
@@ -42,7 +105,7 @@ function createWindow() {
 
 	let watcher;
 
-    if (process.env.NODE_ENV === 'development') {
+    if (mode === 'development') {
         watcher = require('chokidar').watch(path.join(__dirname, '../public/bundle.js'), { ignoreInitial: true });
         watcher.on('change', () => {
             mainWindow.reload();
@@ -51,7 +114,24 @@ function createWindow() {
 
 	mainWindow.loadURL(`file://${path.join(__dirname, '../public/index.html')}`);
 
-	mainWindow.on('close', function(e){
+	mainWindow.on('ready-to-show', () => {
+		mainWindow.show();
+		mainWindow.focus();
+	});
+
+	['resize', 'move', 'close'].forEach((e) => {
+		mainWindow.on(e, () => {
+			windowState.isMaximized = mainWindow.isMaximized();
+
+			if(!windowState.isMaximized){
+				windowState.bounds = mainWindow.getBounds();
+			}
+
+			settings.set('windowState', windowState);
+		});
+	});
+
+	mainWindow.on('close', (e) => {
 		if(mainWindow.isDocumentEdited()){
 			var choice = require('electron').dialog.showMessageBoxSync(this, {
 				type: 'question',
@@ -65,9 +145,10 @@ function createWindow() {
 	});
 
 	mainWindow.on('closed', () => {
-        if (watcher) {
+        if(watcher){
             watcher.close();
-        }
+		}
+
         mainWindow = null;
 	});
 }
